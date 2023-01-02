53416cb78de8bfd84442abb1603f736f3865fcc5
fatemeh zamanipour <f.zamanipour@gmail.com>
2023-01-02 18:58:54 +0330

    add backend

--- client/script.js
+++ client/script.js
@@ -65,7 +65,7 @@ const handleSubmit = async(e)=>{
 
   loader(messageDiv)
   // fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://ai-chat-bq65.onrender.com', {
     method: 'POST',
     headers:{
       "Content-Type": 'application/json'
